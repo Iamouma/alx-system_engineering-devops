164d60f646ed22bd995c7c74e5ccfed0368ce460
Iamouma <stephenouma452@gmail.com>
2024-05-07 03:05:46 -0700

    Minor changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""This queries Reddit"""
+"""This queries reddit API and return number of subscribers"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """This queries a subreddit and retrieve number of subcribers"""
-
-    # Reddit API endpoint for getting subreddit information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Send a custom user-agent to avoid too many requests error
-    headers = {'User-agent': 'My user Agent 1.0'}
-
-    # Send a GET request to the reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request is successful and not a redirect
-    if response.status_code == 200:
-        # Parse JSON response to extract number of subcribers
-        data = response.json().get('data', {})
-        sub_count = data.get('subscribers', 0)
-        return sub_count
-    else:
+    """This returns number of subscribers """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'This Agent?'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 404:
         return 0
+    return r.json().get('data').get('subscribers')
+
+
+if __name__ == '__main__':
+    number_of_subscribers(subreddit)
